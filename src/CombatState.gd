487d7e76458049bec5645dcf0d803fa32ce73c58
dukebot <danielfg1990@gmail.com>
2021-05-11 23:28:52 +0200

    Mejoras en el estado del combate.

--- src/CombatState.gd
+++ src/CombatState.gd
@@ -12,24 +12,26 @@ onready var main = get_parent()
 
 func process_state():
 	var player = main.player
-	var skills = main.skills
 	var map = main.map
 	
 	if turn == 0:
-		show_player_attack_selection()
+		show_attack_selection(player)
 		turn = 1
 	
 	elif turn == 1:
-		var skill_key = get_player_skill_selection()
+		var skill_key = get_skill_selection(player)
 		if skill_key:
 			attack(player, skill_key, enemy)
+			if enemy["health"] < 0:
+				map[enemy_x][enemy_y] = " "
 			turn = 2
 	
 	elif turn == 2:
-		main.show_message(last_attack_result_text)
+		main.show_message(last_attack_result_text + "\nPress SPACE to continue")
 		if Input.is_action_just_pressed("continue"):
-			if map[enemy_x][enemy_y] == " ":
+			if enemy["health"] < 0:
 				main.set_move_state()
+				main.show_message("You've defeated " + enemy["name"])
 			else:
 				turn = 3
 
@@ -38,18 +40,21 @@ func process_state():
 		var rand_skill_index = Utils.get_random_array_index(enemy_skill_keys)
 		var skill_key = enemy_skill_keys[rand_skill_index]
 		attack(enemy, skill_key, player)
+		if player["health"] < 0:
+			print("Game Over...")
+			main.set_move_state()
+			main.show_message("You've been defeated...")
 		turn = 4
 	
 	elif turn == 4:
-		main.show_message(last_attack_result_text)
+		main.show_message(last_attack_result_text + "\nPress SPACE to continue")
 		if Input.is_action_just_pressed("continue"):
 			turn = 0
 
 
-func show_player_attack_selection():
-	var player = main.player
-	
+func show_attack_selection(player: Dictionary):
 	var text = "Select your attack:"
+	
 	var i = 0
 	for skill in player["skills"]:
 		i += 1
@@ -58,10 +63,9 @@ func show_player_attack_selection():
 	main.show_message(text)
 
 
-func get_player_skill_selection():
-	var player = main.player
-	
+func get_skill_selection(player: Dictionary):
 	var skill_key
+	
 	if Input.is_action_just_pressed("skill_1"):
 		skill_key = player["skills"][0]
 	elif Input.is_action_just_pressed("skill_2"):
@@ -73,8 +77,7 @@ func get_player_skill_selection():
 
 
 func attack(attacker: Dictionary, skill_key: String, target: Dictionary):
-	var skills = main.skills
-	var skill = skills[skill_key]
+	var skill = main.skills[skill_key]
 	var damage = attacker["strength"] + skill["power"] - target["defence"]
 	
 	if rand_range(0, 1) < skill["accuracy"]:
@@ -82,10 +85,3 @@ func attack(attacker: Dictionary, skill_key: String, target: Dictionary):
 		last_attack_result_text = attacker["name"] + " used " + skill["name"] + " and inflicted " + str(damage) + " of damage"
 	else:
 		last_attack_result_text = attacker["name"] + " used " + skill["name"] + " and missed!"
-	
-	if target["health"] < 0:
-		if target["name"] == "Player":
-			print("Game Over...")
-		else:
-			var map = main.map
-			map[enemy_x][enemy_y] = " "
